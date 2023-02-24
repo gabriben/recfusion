2ecb327687af593c2cc34e0f3c2eea8dae040437
Gabriel Bénédict <gbndict@gmail.com>
2023-02-24 16:48:50 +0100

    remove if wandb run is not none

--- recpack/pipelines/pipeline.py
+++ recpack/pipelines/pipeline.py
@@ -217,11 +217,9 @@ class Pipeline(object):
             df.index = df.index.map(lambda x: x.split("(")[0])
 
         # wb = {m : v for m,v in zip(df.columns, df.values[0])}
-        if wandb.run is not None:
-            for m, v in zip(df.columns, df.values[0]):
-                wandb.run.summary[m] = v
-            
-        wandb.finish()
+        # if wandb.run is not None:
+        for m, v in zip(df.columns, df.values[0]):
+            wandb.run.summary[m] = v
         
         return df
 
